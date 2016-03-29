2a800ae22b190cc9ec5f5e60547ce3d735b4f6cc
etheryom <etheryom@etheryom-desktop>
2016-03-29 13:54:51 +0200

    FlowLayout del tema 12

--- Programacion3º/Programacion3ºTrimestre/tema12Clases/FlowLayout.java
+++ Programacion3º/Programacion3ºTrimestre/tema12Clases/FlowLayout.java
@@ -1,5 +1,18 @@
 package tema12Clases;
+import javax.swing.*;
 
-public class FlowLayout {
+public class FlowLayout extends JFrame {
+	
+	public FlowLayout(){
 
+		//Lo coloco con el import, ya que arriba me lo toma como obsoleto.
+		setLayout(new java.awt.FlowLayout(0, 10, 20));
+		
+	    add(new JLabel("Su nombre"));
+	    add(new JTextField(16));
+	    add(new JLabel("Edad"));
+	    add(new JTextField(2));
+	    add(new JLabel("Apellidos"));
+	    add(new JTextField(16));
+	}
 }
--- Programacion3º/Programacion3ºTrimestre/tema12Test/TestFlowLayout.java
+++ Programacion3º/Programacion3ºTrimestre/tema12Test/TestFlowLayout.java
@@ -1,10 +1,19 @@
 package tema12Test;
 
+import javax.swing.JFrame;
+
+import tema12Clases.FlowLayout;
+
 public class TestFlowLayout {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		FlowLayout miPanel = new FlowLayout();
+		
+		miPanel.setSize(400,200);
+		miPanel.setVisible(true);
+		miPanel.setTitle("Flow Layout");
+		miPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
