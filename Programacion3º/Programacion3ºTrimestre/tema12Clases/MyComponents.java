3778c979d3b0bfeb1a87032a288ed16ba1925945
etheryom <etheryom@etheryom-desktop>
2016-04-05 13:37:35 +0200

    My Components terminado

--- Programacion3º/Programacion3ºTrimestre/tema12Clases/MyComponents.java
+++ Programacion3º/Programacion3ºTrimestre/tema12Clases/MyComponents.java
@@ -1,22 +1,71 @@
 package tema12Clases;
 import java.awt.*;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
 public class MyComponents extends JFrame{
 	
-	MyComponents(){
+	public MyComponents(){
+		
+	//-----<<>>PANEL 1<<>>------
+		
+	//Creo el panel  y los tres botones
 	JPanel arriba = new JPanel();
+	arriba.setLayout(new FlowLayout(0));
 	JButton left = new JButton("Left");
 	JButton center = new JButton("Center");
 	JButton right = new JButton("Right");
-	arriba.add(arriba);
+	
+	//Añado los botones al JPanel
+	arriba.add(left);
 	arriba.add(center);
 	arriba.add(right);
 	
-	add(arriba);
+	//Doy color al boton center
+	center.setForeground(new Color(0,255,0));
 	
+	//Creo el border con color y titulo
+	arriba.setBorder(new LineBorder(new Color(102,255,255), 3));
+	arriba.setBorder(new TitledBorder("Three Buttons"));
+	
+	
+	
+	//-----<<>>PANEL 2<<>>------
+	
+	//Creo el panel de abajo con un gestor GridLayout para que tome todo el ancho de la columna y los dos JLabel
+	JPanel abajo = new JPanel();
+	abajo.setLayout(new GridLayout(1,2,2,0));
+	JLabel red = new JLabel("Red");
+	JLabel orange = new JLabel("Orange");
+	
+	//Agrego los JLabel al JPanel
+	abajo.add(red);
+	abajo.add(orange);
+	
+	//Cambio las fuentes y color de los JLabels
+	red.setForeground(new Color(255,0,0));
+	orange.setForeground(new Color(255,128,0));
+	
+	red.setFont(new Font("SansSerif ", Font.BOLD, 18));
+	orange.setFont(new Font("SansSerif ", Font.BOLD, 18));
+	
+	//Asigno los bordes a los JLABELS
+	red.setBorder(new LineBorder(new Color(0,0,0),2));
+	orange.setBorder(new LineBorder(new Color(0,0,0),2));
+	
+	//Asigno Borde y color al JPanel
+	abajo.setBorder(new LineBorder(new Color(102,255,255), 3));
+	abajo.setBorder(new TitledBorder("Two Labels"));
+	
+	
+	//Añado los dos JPanel al frame
+	add(arriba,BorderLayout.NORTH);
+	add(abajo,BorderLayout.CENTER);
 	}
 }
--- Programacion3º/Programacion3ºTrimestre/tema12Test/TestMyComponents.java
+++ Programacion3º/Programacion3ºTrimestre/tema12Test/TestMyComponents.java
@@ -1,9 +1,21 @@
 package tema12Test;
 
+import javax.swing.JFrame;
+
+import tema12Clases.MyComponents;
+
 public class TestMyComponents {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		MyComponents miPanel = new MyComponents();
+		miPanel.setTitle("My Components");
+		miPanel.setSize(300,140);
+		miPanel.setVisible(true);
+		miPanel.setResizable(false);
+		miPanel.setLocationRelativeTo(null);
+		miPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 	}
 
