da771cf3832a54d451e902af3b321158f19dcecb
etheryom <etheryom@etheryom-desktop>
2016-04-08 16:18:44 +0200

    mejorado FigurePanel

--- Programacion3º/Programacion3ºTrimestre/Tema13Test/TestFigurePanel.java
+++ Programacion3º/Programacion3ºTrimestre/Tema13Test/TestFigurePanel.java
@@ -1,6 +1,9 @@
 package Tema13Test;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import Tema13Clases.FigurePanel;
 
@@ -8,11 +11,20 @@ import Tema13Clases.FigurePanel;
 public class TestFigurePanel extends JFrame {
 
 	public TestFigurePanel() {
-		FigurePanel panel1 = new FigurePanel(FigurePanel.OVAL,true);
-		System.out.println(panel1.getType());
+		
+		int num = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Cuantas figuras deseas crear?"));
+
+		for(int i=0;i<num;i++){
+			int tipo = Integer.parseInt(JOptionPane.showInputDialog(null,"Selecciona una:\n1.-Recta\n2.-Rectangulo\n3.-Rectangulo redondeado\n4.-Ovalo"));
+			boolean fil = Boolean.parseBoolean(JOptionPane.showInputDialog(null,"¿Estará rellena la figura?\n True o False"));
+			add(new FigurePanel(tipo,fil));
+			
+		}
+		setLayout(new GridLayout((num/4)+1,4,5,5));
+		//System.out.println(panel1.getType());
 		//panel1.setType(2);
 		//panel1.setFilled(false);
-		add(panel1);
+		
 		
 	}
 		
