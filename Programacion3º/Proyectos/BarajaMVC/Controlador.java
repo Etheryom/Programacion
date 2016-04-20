a5eef213fd2edd678fc55368b034b54ada6d3894
etheryom <etheryom@etheryom-desktop>
2016-04-21 00:16:32 +0200

    - Añadido baraja al github - Cambio de adiccion al panel principal contenedora de cartas, al añadir 4 paneles mas.

--- Programacion3º/Proyectos/BarajaMVC/Controlador.java
+++ Programacion3º/Proyectos/BarajaMVC/Controlador.java
@@ -13,6 +13,7 @@ public class Controlador implements ActionListener {
 		vista.getDarCarta().addActionListener(this);
 	}
 	
+	/*Antiguo metodo para dar las cartas en un solo gridLayout
 	public void actionPerformed(ActionEvent e) {
 		if(vista.getBaraja().getIndice()<=39){
 			vista.addCarta();
@@ -26,6 +27,31 @@ public class Controlador implements ActionListener {
 				vista.setCartas();
 			if(vista.getBaraja().getIndice()==40)
 				vista.getDarCarta().setText("No quedan mas cartas");
+		}
+					
+	}*/
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		if(vista.getBaraja().getIndice()<=39){
+			
+			if(vista.getBaraja().getIndice()>29 && vista.getBaraja().getIndice()<=39)
+				vista.getFila4().add(vista.getBaraja().darCarta());
+			
+			if(vista.getBaraja().getIndice()>19 && vista.getBaraja().getIndice()<=29)
+				vista.getFila3().add(vista.getBaraja().darCarta());
+			
+			if(vista.getBaraja().getIndice()>9 && vista.getBaraja().getIndice()<=19)
+				vista.getFila2().add(vista.getBaraja().darCarta());
+			
+			if(vista.getBaraja().getIndice()<=9)
+				vista.getFila1().add(vista.getBaraja().darCarta());
+			
+			if(vista.getBaraja().getIndice()==40)
+				vista.getDarCarta().setText("No quedan mas cartas");
+			
+			//Actualiza toda el frame
+			vista.paintAll(vista.getGraphics());
 		}
 					
 	}
--- Programacion3º/Proyectos/BarajaMVC/Vista.java
+++ Programacion3º/Proyectos/BarajaMVC/Vista.java
@@ -10,10 +10,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Vista extends JFrame{
-	private static int filas_grid=1;
+	/*Antigua propiedad para las filas del grid
+	private static int filas_grid=1;*/
 	
 	private JPanel cartas;
 	private JPanel boton;
+	
+	private JPanel fila1;
+	private JPanel fila2;
+	private JPanel fila3;
+	private JPanel fila4;
+	
 	private JButton darCarta;
 	
 	private ModBaraja baraja = new ModBaraja();
@@ -22,7 +29,21 @@ public class Vista extends JFrame{
 		baraja.barajar();
 		setLayout(new BorderLayout());
 		
+		/*Antiguo Modo de comportamiento
 		cartas = new JPanel(new GridLayout(1,10));
+		*/
+		
+		cartas = new JPanel(new GridLayout(4,1));
+		fila1 = new JPanel(new GridLayout(1,10));
+		fila2 = new JPanel(new GridLayout(1,10));
+		fila3 = new JPanel(new GridLayout(1,10));
+		fila4 = new JPanel(new GridLayout(1,10));
+		
+		cartas.add(fila1);
+		cartas.add(fila2);
+		cartas.add(fila3);
+		cartas.add(fila4);
+		
 		darCarta=new JButton("Dar Carta");
 		boton = new JPanel(new FlowLayout(1));
 		boton.setBackground(Color.BLACK);
@@ -33,13 +54,28 @@ public class Vista extends JFrame{
 		
 	}
 	
+	public JPanel getFila1(){
+		return fila1;
+	}
+	public JPanel getFila2(){
+		return fila2;
+	}
+	public JPanel getFila3(){
+		return fila3;
+	}
+	public JPanel getFila4(){
+		return fila4;
+	}
+	
 	public JPanel getCartas(){
 		return cartas;
 	}
 	
+	/*Metodo antiguo de visualizacion
 	public void setCartas(){
 		cartas.setLayout(new GridLayout(++filas_grid,10));
-	}
+	}*/
+	
 	
 	public JPanel getBoton(){
 		return boton;
@@ -49,9 +85,11 @@ public class Vista extends JFrame{
 		return darCarta;
 	}
 	
+	/*Antiguo: ya no requiero que me lo añada desde aqui, lo hago desde el controlador.
 	public void addCarta(){
 		cartas.add(baraja.darCarta());
-	}
+	}*/
+	
 	public ModBaraja getBaraja(){
 		return baraja;
 	}
